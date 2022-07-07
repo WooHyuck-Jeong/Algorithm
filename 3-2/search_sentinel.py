e6e9780545b74bb738baa598a2752aa95af4d9e8
WooHyuck-Jeong <jwhyukk10@gmail.com>
2022-07-07 17:10:18 +0900

    Chapter 3-3 Fin

--- 3-2/search_sentinel.py
+++ 3-2/search_sentinel.py
@@ -7,25 +7,25 @@ import copy
 def seq_search(seq : Sequence, key : Any) -> int :
     """시퀀스 seq에서 key와 일치하는 원소를 선형 검색 (보초법)"""
     
-    a = copy.deepcopy(seq)                  # seq를 복사
-    a.append(key)                           # 보초 key를 추가
+    a = copy.deepcopy(seq)                      # seq를 복사
+    a.append(key)                               # 보초 key를 추가
     
     i = 0
     
     while True :
         if a[i] == key:
-            break
+            break                               # 성공하면 while문 종료
         i += 1
     return -1 if i == len(seq) else i
 
 if __name__ == '__main__':
-    num = int(input('원소 수 : '))
-    x = [None] * num
+    num = int(input('원소 수 : '))                  # 원소 수 입력
+    x = [None] * num                            # 원소 수가 num인 배열을 생성
     
     for i in range(num):
-        x[i] = int(input(f'x[{i}] : '))
-        
-    ky = int(input('검색 값 : '))
+        x[i] = int(input(f'x[{i}] : ')) 
+                
+    ky = int(input('검색 값 : '))               # 검색할 key입력
     
     idx = seq_search(x, ky)
     
