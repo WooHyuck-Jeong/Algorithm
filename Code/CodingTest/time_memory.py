8d4bd1e4d87ac102eda485a487ea9bcdc2eee2c5
WooHyuck-Jeong <jwhyukk10@gmail.com>
2022-07-12 20:16:52 +0900

    CT-Chapter1 Fin

--- Code/CodingTest/time_memory.py
+++ Code/CodingTest/time_memory.py
@@ -1,6 +1,14 @@
 import time
 start_time = time.time()    # 측정 시작
 
-# 프로그램 소스 코드
+"""시간 측정 예제"""
+a = [1, 5, 6, 3, 2]
+s = 0
+
+for i in a:
+    s += i
+print(s)
+"""시간 측정 예제"""
+
 end_time = time.time()      # 측정 종료
 print("time : ", end_time - start_time)     # 수행시간 출력
